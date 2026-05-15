056090a649c56b6cde331d9e13136ba92d2fa818
alex <alexbissessur@gmail.com>
2026-05-15 14:14:19 +0400

    add step for barman install

--- manifests/cnpostgres/install.sh
+++ manifests/cnpostgres/install.sh
@@ -1,2 +1,5 @@
 kubectl apply --server-side -f \
-  https://raw.githubusercontent.com/cloudnative-pg/cloudnative-pg/release-1.25/releases/cnpg-1.25.1.yaml
+    https://raw.githubusercontent.com/cloudnative-pg/cloudnative-pg/release-1.29/releases/cnpg-1.25.1.yaml
+
+kubectl apply -f \
+    https://github.com/cloudnative-pg/plugin-barman-cloud/releases/download/v0.12.0/manifest.yaml
